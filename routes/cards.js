4605ad49e68fd9e74b6a76f9084232be2474d283
Svirriill <sverelb@bk.ru>
2020-10-09 11:05:40 +0300

    express-mesto

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,18 @@
 const routesCards = require('express').Router();
-const cards = require('../data/cards.json');
+const path = require('path');
+const getJson = require('../read-file');
 
-routesCards.get('/', (req, res) => {
-  res.send(cards);
-});
+const cards = (req, res) => (
+  getJson(path.join(__dirname, '..', 'data', 'cards.json'))
+  .then((data) => {
+    if (!data) {
+      res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+      return;
+    }
+    res.status(200).send(data);
+  })
+)
+
+routesCards.get('/', cards);
 
 module.exports = routesCards;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,37 @@
 const routesUser = require('express').Router();
-const users = require('../data/users.json');
+const getJson = require('../read-file');
+const path = require('path');
 
-routesUser.get('/', (req, res) => {
-  res.send(users);
-});
+const users = (req, res) => (
+  getJson(path.join(__dirname, '..', 'data', 'users.json'))
+  .then((data) => {
+    if (!data) {
+      res.status(500).send({ message:'Ошибка сервера' });
+      return;
+    }
+    res.status(200).send(data);
+  })
+)
 
-routesUser.get('/:id', (req, res) => {
-  const user = users.find((item) => item._id === req.params.id);
+const user = (req, res) => (
+  getJson(path.join(__dirname, '..', 'data', 'users.json'))
+  .then((data) => {
+    if (!data) {
+      res
+        .status(500)
+        .send({ message: 'Ошибка сервера' });
+      return;
+    }
+  const user = data.find((item) => item._id === req.params.id);
   if (user) {
     res.send(user);
   } else {
     res.status(404).send({ message: 'Нет пользователя с таким id' });
   }
-});
+})
+)
+
+routesUser.get('/', users);
+routesUser.get('/:id', user);
 
 module.exports = routesUser;
